d9ad3b4c9ef54d9eb74119885919a96b0e79fb6f
Bergi <info@bergstreisser.de>
2024-05-01 20:45:23 +0200

    mp3 Player added

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import Playback from "./components/Playback";
 function App() {
   return (
     <div>
-      <div className="header"><Header /></div>
+      <div className="header">
+        <Header />
+      </div>
       <div className="wrapper">
         <div className="content">
           <h1>Alle MP3-Playbacks</h1>
-          <Playback />
-          <Playback />
-          <Playback />
+          <div className="playbacks">
+            <Playback />
+            <Playback />
+            <Playback />
+            <Playback />
+          </div>
         </div>
       </div>
     </div>
